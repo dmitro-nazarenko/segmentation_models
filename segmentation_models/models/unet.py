fed515e33144ec2b65f886a21aa950e07459da8e
Dmitry Nazarenko <dmitro.nazarenko@gmail.com>
2019-10-16 18:23:31 +0300

    return 1 attn

--- segmentation_models/models/unet.py
+++ segmentation_models/models/unet.py
@@ -153,7 +153,7 @@ def build_unet(
             skip = None
 
         x = decoder_block(decoder_filters[i], stage=i, use_batchnorm=use_batchnorm)(x, skip)
-        if i < 2 and attention:
+        if i == 1 and attention:
             pam = PAM()(x)
             pam = Conv2D(decoder_filters[i], 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
             pam = BatchNormalization(axis=3)(pam)
